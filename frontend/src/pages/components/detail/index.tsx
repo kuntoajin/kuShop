e3ca4ac501c5cfa2f7ecf8ad1d690a50d9a40f97
kuntoajin <kuntoaji.engineer@gmail.com>
2022-08-11 17:49:03 +0700

    push

--- frontend/src/pages/components/detail/index.tsx
+++ frontend/src/pages/components/detail/index.tsx
@@ -10,6 +10,10 @@ interface Data {
     id: number
     image: string
     title: string
+    price: number
+    rating?: {
+        rate?: number
+    }
 }
 
 const Detail: React.FC<PropData> = ({datas}): JSX.Element => {
@@ -18,9 +22,7 @@ const Detail: React.FC<PropData> = ({datas}): JSX.Element => {
     let {id} = useParams()
 
     useEffect(() => {
-        // console.log(data)
         setSelectedProduct(datas?.find(data => data['id'] == id))
-        // console.log(test)
     }, [datas])
 
     const handlePost = async () => {
@@ -29,24 +31,76 @@ const Detail: React.FC<PropData> = ({datas}): JSX.Element => {
             first_name: "Gunawan",
             last_name: "Sulis"
         })
-        console.log(response)
         setToken(response?.data?.token)
     }
 
     useEffect(() => {
-        console.log(token)
         token && window?.snap?.pay(token)
-        // console.log(window.snap.pay(token))
     }, [token])
+
+    const ratingProduct: undefined | number = selectedProduct?.rating?.rate && Math.floor(selectedProduct?.rating?.rate)
+    let starRating:JSX.Element[] = []
+    let decilmalRating: JSX.Element[] = []
+    let noneRating:JSX.Element[] = []
+    let showRating: number | undefined | string
+
+    if(ratingProduct) {
+        for(let i = 1; i <= ratingProduct; i++) {
+            starRating.push(<svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 fill-yellow-500" viewBox="0 0 20 20">
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>)
+        }
+    }
+
+    if(ratingProduct) {
+        const decimalRatingProduct = selectedProduct?.rating?.rate && selectedProduct?.rating?.rate - ratingProduct
+        const getLastRatingProduct = decimalRatingProduct && Math.ceil(decimalRatingProduct*100)
+        decilmalRating.push(<svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20">
+              <linearGradient id="gradient-star-decimal">
+            <stop stopColor="#eab308" offset={`${getLastRatingProduct}%`} />
+            <stop stopColor="#64748b" offset={`${getLastRatingProduct}%`} />
+        </linearGradient>
+        <path className="star-rating-decimal" d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+    </svg>)
+    }
+    if(ratingProduct) {
+        for(let i = 1; i <= (4-ratingProduct); i++) {
+            noneRating.push(<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-slate-500" fill="none" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+            </svg>)
+        }
+    }
+
+    if(selectedProduct?.rating?.rate && Number.isInteger(selectedProduct?.rating?.rate)) {
+        showRating = selectedProduct?.rating?.rate && selectedProduct?.rating?.rate.toFixed(1)
+    } else {
+        showRating = selectedProduct?.rating?.rate
+    }
 
     return (
         <div className="content">
             <h1>KuShop</h1>
             <input type="text" />
-            <div>
-                <p>{selectedProduct?.title}</p>
-                <img src={selectedProduct?.image} alt="" width='100' height='100' />
-                <button className="bg-cyan-500 p-2" onClick={handlePost}>beli</button>
+            <div className="flex">
+                <div className="h-96 w-80 border-4 rounded-lg border-blue-500 flex justify-center items-center">
+                    <div>
+                        <img src={selectedProduct?.image} alt="" className='w-fit w-44 mx-auto leading-[3rem]' />
+                    </div>
+                </div>
+                <div className="mx-8 border-r border-black">
+                    <h1 className="text-4xl font-bold">{selectedProduct?.title}</h1>
+                    <div className="flex items-center mt-3">
+                        <p className="text-4xl font-bold text-blue-500">${selectedProduct?.price}</p>
+                        <div className="flex ml-6">{starRating}</div>
+                        <div className="flex">{decilmalRating}</div>
+                        <div className="flex">{noneRating}</div>
+                        <p className="text-lg ml-3">{`(${showRating})`}</p>
+                    </div>
+                </div>
+                <div className="w-52">
+                    <button className="bg-blue-500 p-2 w-full text-white font-bold tracking-wider rounded-lg mb-4 py-3" onClick={handlePost}>Tambah Keranjang</button>
+                    <button className="bg-blue-500 p-2 w-full text-white font-bold tracking-wider rounded-lg py-3" onClick={handlePost}>Beli</button>
+                </div>
             </div>
         </div>
     )
--- frontend/src/pages/components/home/index.tsx
+++ frontend/src/pages/components/home/index.tsx
@@ -35,9 +35,9 @@ const Home: React.FC<PropData> = ({datas}): JSX.Element => {
     
     return (
         <div className="content">
-            <header className="flex justify-center py-8 bg-blue-500">
+            <header className="flex justify-center py-8 bg-blue-500 items-center">
                 <h1 className='mx-3'>KuShop</h1>
-                <ul className='mx-3'>Kategori</ul>
+                <ul className='mx-3 font-bold text-white'>Kategori</ul>
                 <input type="text" className="w-7/12 h-10 outline-0 drop-shadow-md px-3 rounded-md" />
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
